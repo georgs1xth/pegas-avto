fc9215f2f8c9d303b2f7338f682e131bda67a9fb
georgf1rst <zhorik06bondarenko@gmail.com>
2024-06-26 18:55:20 +0500

    add search

--- app/(main)/(routes)/catalog/page.tsx
+++ app/(main)/(routes)/catalog/page.tsx
@@ -1,7 +1,11 @@
+import { SearchInput } from "@/components/search-input";
+
 const CatalogPage = () => {
     return ( 
-        <div>
-            This is Catalog page!
+        <div className="p-2 py-4 md:p-4 xl:p-6 flex flex-col gap-y-4">
+            <div className="px-2 md:hidden">
+                <SearchInput/>
+            </div>
         </div>
      );
 }
--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -1,18 +1,31 @@
+"use client";
+
 import NavbarRoutes from "@/components/navbar-routes";
 import { Logo } from "./logo";
 import MobileSidebar from "./mobile-sidebar";
+import { SearchInput } from "@/components/search-input";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+
+    const pathname = usePathname();
+
+    const isCatalogPage = pathname.includes("/catalog");
+
     return ( 
         <div className="p-4 border-b h-full flex items-center bg-white shadow-sm w-full">
 
-            <div className="hidden py-28 justify-center w-100px md:scale-125 md:w-[180px] xl:scale-150 xl:w-[240px] md:flex">
+            <div className="hidden justify-center w-100px md:scale-125 md:w-[180px] xl:scale-150 xl:w-[240px] md:flex">
                 <Logo />
             </div>
-
+            {isCatalogPage && (
+                <div className="ml-10 hidden md:block">
+                    <SearchInput/>
+                </div>
+            )}
             <MobileSidebar/>
             
-            <div className="py-28 justify-center md:hidden flex ml-4">
+            <div className="justify-center md:hidden flex ml-4">
                 <Logo />
             </div>
 
