a991223e747d8259c5c4d3ac9163f0c6ece4a36c
georgf1rst <zhorik06bondarenko@gmail.com>
2024-06-26 14:24:00 +0500

    вскрытие slide

--- app/(main)/(routes)/(root)/_components/main-carousel.tsx
+++ app/(main)/(routes)/(root)/_components/main-carousel.tsx
@@ -23,6 +23,7 @@ const MainCarousel = () => {
                     <CarouselItems imageSrc="/slider-images/Starline.jpeg" imageAlt="установка starline" classes="" btnHref="/catalog/signalisations/starline" itemTitle="StarLine — надежная защита вашего автомобиля." itemDescription="Установка Автосигнализаций."/>
                     <CarouselItems imageSrc="/slider-images/ac-automobile.jpg" imageAlt="заправка автокондиционеров" classes="" btnHref="/services/auto-air-conditioning" itemTitle="Не дайте жаре помешать вашему комфорту!" itemDescription="Заправка и ремонт Автокондиционеров."/>
                     <CarouselItems imageSrc="/slider-images/diagnostics2.jpeg" imageAlt="компьютерная диагностика" classes="" btnHref="/services/computer-diagnostics" itemTitle="Выявляем проблемы авто компьютерной диагностикой." itemDescription="Компьютерная диагностика авто."/>
+                    <CarouselItems imageSrc="/slider-images/vskrytiye.jpg" imageAlt="вскрытие и пр" classes="" btnHref="/services/computer-diagnostics" itemTitle="Оставили ключи в машине или сел аккумулятор? Не беда" itemDescription="Аварийное вскрытие и прикуривание авто."/>
                 </CarouselContent>
             </Carousel>
      );
