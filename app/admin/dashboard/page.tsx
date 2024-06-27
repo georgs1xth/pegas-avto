2ac4e04412023bb07117f583122c881353bf9e11
georgf1rst <zhorik06bondarenko@gmail.com>
2024-06-28 00:59:40 +0500

    db change

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -24,8 +24,8 @@ export default async function AdminDashboard(params: {
   return (
     <>
       <h1>This is the admin dashboard</h1>
-      <p>This page is restricted to users with the 'admin' role.</p>
-      
+      <p>This page is restricted to users with the admin role.</p>
+
       <SearchUsers />
 
       {users.map((user) => {
