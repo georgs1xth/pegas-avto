d9d99444a1c534faa24fde362e154a2620ea558c
georgf1rst <zhorik06bondarenko@gmail.com>
2024-07-09 16:49:16 +0500

    add appointment btn

--- app/(main)/(routes)/employee/schedule/page.tsx
+++ app/(main)/(routes)/employee/schedule/page.tsx
@@ -3,6 +3,8 @@ import { DataTable } from "./data-table"
 import { Columns } from "./columns"
 import { Appointment } from "@prisma/client"
 import db from "@/lib/db"
+import { Button } from "@/components/ui/button"
+import { useRouter } from "next/navigation"
 
 // export type Payment = {
 //     id: string
@@ -16,9 +18,17 @@ import db from "@/lib/db"
 const SchedulePage = async () => {
 
   const data = await db.appointment.findMany()
+  const router = useRouter();
+
+  const onClick = () => {
+    router.push('/employee/schedule/create/appointment')
+  }
 
   return (
     <div className="p-4">
+        <Button onClick={onClick}>
+          Добавить запись
+        </Button>
         <DataTable columns={Columns} data={data}/>
     </div>
   )
