61edddb28091ca72889ff3f1f83e218652063623
georgf1rst <zhorik06bondarenko@gmail.com>
2024-08-01 01:31:34 +0500

    mode

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -24,7 +24,7 @@ children: React.ReactNode
   return (
       <>
         <div className="flex gap-1">
-            <ModeToggle/>
+            {/* <ModeToggle/> */}
             <Popover>
                 <PopoverTrigger>
                     <div className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 px-2.5 py-2">
