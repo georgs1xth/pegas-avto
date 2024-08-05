e4015d1da7df7b9bcdf54e017256017a0f1c74b6
georgf1rst <zhorik06bondarenko@gmail.com>
2024-08-05 13:15:51 +0500

    some ui fixes

--- app/(main)/_components/sidebar.tsx
+++ app/(main)/_components/sidebar.tsx
@@ -5,8 +5,7 @@ import SidebarRoutes from "./sidebar-routes";
 const Sidebar = () => {
     return ( 
         <div className="h-full w-full flex flex-col overflow-y-auto md:bg-background shadow-sm">
-            <div className="flex flex-col w-full pb-2 gap-2">
-                
+            <div className="flex flex-col w-full pb-2 gap-2 md:py-2">
                 <SidebarRoutes />
             </div>
         </div>
