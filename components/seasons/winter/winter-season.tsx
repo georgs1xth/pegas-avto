60bea3ed286bb8918e03cfbe1dffab5cd1d595f9
georgf1rst <zhorik06bondarenko@gmail.com>
2024-08-09 15:52:47 +0500

    winter adaptation

--- components/seasons/winter/winter-season.tsx
+++ components/seasons/winter/winter-season.tsx
@@ -1,15 +1,18 @@
+
 import { Button } from "@/components/ui/button"
 import Image from "next/image"
 
 const WinterSeason = () => {
   return (
-    <div className="flex flex-col gap-4 relative w-full min-h-max h-[calc(100svh-60px)] overflow-hidden pb-5">
+    <div className="flex flex-col w-full h-[calc(100svh-75px)] overflow-hidden md:hidden">
         <h2 className="text-3xl font-medium">
-            Надежная защита и умный комфорт уже доступны для вашего автомобиля
+            Надежная защита и умный комфорт в ваш автомобиль
         </h2>
-        <Image src="/seasons/winter/StarLineE96v2.png" alt="фото сезонного баннера" width={400} height={400} className="object-contain scale-150 -rotate-90 w-full h-full"/>
-        <Button variant="default" size="lg" className="text-lg rounded-xl">
-            Записаться
+        <div className="flex-grow relative">
+          <Image src="/seasons/winter/StarLineE96v2.png" alt="фото сезонного баннера" fill className="object-contain"/>
+        </div>
+        <Button variant="default" size="lg" className="text-lg rounded-xl w-full">
+            Подобрать автосигнализацию
         </Button>
     </div>
   )
