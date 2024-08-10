c71821a3b3351eafb5df9e5044bdb344cc860a90
georgf1rst <zhorik06bondarenko@gmail.com>
2024-08-10 14:59:20 +0500

    theme rusification

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { Moon, Sun } from "lucide-react"
+import { Computer, Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 
 import { Button } from "@/components/ui/button"
@@ -21,18 +21,18 @@ export function ModeToggle() {
         <Button variant="outline" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
+          <span className="sr-only">Изменить тему</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
+        <DropdownMenuItem onClick={() => setTheme("light")} className="flex gap-2">
+          <Sun className="h-[1.2rem] w-[1.2rem]"/> Светлая
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
+        <DropdownMenuItem onClick={() => setTheme("dark")} className="flex gap-2">
+          <Moon className="h-[1.2rem] w-[1.2rem]"/> Темная
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
+        <DropdownMenuItem onClick={() => setTheme("system")} className="flex gap-2">
+          <Computer className="h-[1.2rem] w-[1.2rem]"/> Системная
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
