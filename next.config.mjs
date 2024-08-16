8a382b0326d0c819c6724234df674150a245aa96
georgf1rst <zhorik06bondarenko@gmail.com>
2024-08-16 15:16:29 +0500

    try again 4

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 const cspHeader = `
+ default-src 'self';
  script-src 'self' 'unsafe-inline' 'unsafe-eval' https://clerk.pegas-avto-a.com https://challenges.cloudflare.com;
  connect-src 'self' https://clerk.pegas-avto-a.com;
- img-src *;
+ img-src 'self' https://img.clerk.com https://uploadthing.com/ https://*.s3.amazonaws.com;
  worker-src 'self' blob:;
  style-src 'self' 'unsafe-inline';
  frame-src 'self' https://challenges.cloudflare.com;
