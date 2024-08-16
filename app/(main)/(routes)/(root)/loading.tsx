44a778fa11db1e80c789e573ca8fb9a78f32e058
georgf1rst <zhorik06bondarenko@gmail.com>
2024-08-16 16:34:55 +0500

    loading fix

--- app/(main)/(routes)/(root)/loading.tsx
+++ app/(main)/(routes)/(root)/loading.tsx
@@ -3,9 +3,6 @@ import { Skeleton } from "@/components/ui/skeleton";
 export default function Loading() {
     return (
         <>
-        <Skeleton className="flex flex-col w-full h-[calc(100svh-75px)] overflow-hidden md:hidden px-4 pt-[5svh] pb-[10svh]">
-            {/* <Skeleton className="size-full"/> */}
-        </Skeleton>
         <div className="p-2 md:p-4 xl:p-6 flex flex-col gap-y-4"> 
             <div className="flex flex-row gap-4">
                 <div className="basis-full md:grid md:basis-1/2 xl:basis-1/3 h-full">
