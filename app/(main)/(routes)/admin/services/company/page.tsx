10626bb302bc6183e04ad83017370a4dc89b5132
Georg.s1xth <zhorik06bondarenko@gmail.com>
2024-12-17 09:28:36 +0500

    beautify

--- app/(main)/(routes)/admin/services/company/page.tsx
+++ app/(main)/(routes)/admin/services/company/page.tsx
@@ -1,3 +1,4 @@
+import { Logo } from "@/app/(main)/_components/logo";
 import { Button } from "@/components/ui/button"
 import db from "@/lib/db"
 import Link from "next/link"
@@ -9,8 +10,19 @@ const AdminCompanyPage = async () => {
   return (
     <div className="p-2 md:p-4 grid grid-cols-1 lg:grid-cols-2 gap-4">
         {companies.map((company) => (
-            <Link key={company.id} href={`/admin/services/company/${company.id}`} className="flex justify-center items-center bg-accent/20 rounded-2xl hover:bg-accent/40 transition-all">
-                {company.name}
+            <Link key={company.id} href={`/admin/services/company/${company.id}`} className="flex justify-center items-center bg-accent/20 rounded-2xl hover:bg-accent/40 transition-all p-8">
+                {company.name == "Пегас avto A" && (
+                    <div className="flex justify-center items-center overflow-hidden bg-accent/20 rounded-xl">
+                        <div className="scale-125">
+                            <Logo/>
+                        </div>
+                    </div>
+                )}
+                {company.name == "Инжектор Сервис" && (
+                    <div className="pb-2 flex justify-center items-center text-2xl bg-accent/20 rounded-xl font-bold text-yellow-800 dark:text-red-700">
+                        Injector Service
+                    </div>
+                )}
             </Link>
         ))}
     </div>
